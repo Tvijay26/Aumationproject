e2431f2e1972a4d13af2115806a7c3e539ba43fc
Tvijay26 <vijaykarthik223344@gmail.com>
2025-11-11 07:53:17 +0530

    git push

---
+++ src/main/java/TECHNOLOGIES/FLIPKART/App.java
@@ -0,0 +1,13 @@
+package TECHNOLOGIES.FLIPKART;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
